0cf230306e4fa64d342384706bd288e28a50d4de
Tonics88 <tcs1988@gmail.com>
2017-12-28 11:00:23 +0100

    Afegida la funcio esBisiesto

--- js/calculdiasetmana.js
+++ js/calculdiasetmana.js
@@ -1,3 +1,13 @@
+function esBisiesto(x){
+    if ( (x.any.substring( (x.any.length-2, x.any.length))%4 && (x.any.substring(x.any.length-2, x.any.length))!="00")
+    || x.any.substring(x.any.length-2, x.any.length)=="00" && (x.any.substring(x.any.length-2, x.any.length)%400)==0){
+        mes.dies[1]=29;
+        return true;
+    }
+    return false
+    
+}
+
 function dataCheck(x,y){
     var mes=y.nom;
     while (x.any<1700 || x.any>2299){
@@ -8,8 +18,20 @@ function dataCheck(x,y){
          //añadir contador de numero de intentos
          x.mes=prompt("Mes no compres entre Gener (1) i Decembre (12) \nIntrodueix un altre mes per favor")
     }
+console.log(esBisiesto(x));
     if (x.mes==2){
-
+        if (esBisiesto(x)){
+            while (x.dia<1 || x.dia>29){
+                //añadir contador de numero de intentos
+                x.dia=prompt("El mes de "+mes[x.mes-1]+" te 29 dies, tu has introduit: "+x.dia+"\nIntrodueix un dia entre 1 i 29 per favor")
+           } 
+        } else {
+            while (x.dia<1 || x.dia>28){
+                //añadir contador de numero de intentos
+                x.dia=prompt("El mes de "+mes[x.mes-1]+" te 28 dies, tu has introduit: "+x.dia+"\nIntrodueix un dia entre 1 i 28 per favor")
+           }
+        }
+        
     } else if (x.mes==4 || x.mes==6 || x.mes==9 || x.mes==11){
         while (x.dia<1 || x.dia>30){
             //añadir contador de numero de intentos
@@ -18,7 +40,7 @@ function dataCheck(x,y){
     }else {
         while (x.dia<1 || x.dia>31){
             //añadir contador de numero de intentos
-            x.dia=prompt("El mes de "+mes[x.mes-1]+" te 31 dies, tu has introduit: "+x.dia+"\nIntrodueix un dia entre 1 i 30 per favor")
+            x.dia=prompt("El mes de "+mes[x.mes-1]+" te 31 dies, tu has introduit: "+x.dia+"\nIntrodueix un dia entre 1 i 31 per favor")
        }
     }
     return x;
@@ -32,10 +54,6 @@ function calendarConstructor(data,mes){
     var x=data.dia;//var dia de la setmana triat
     var y=0;//var que indica dia de la setmana de dia 1 d'aquest mes
     var z=mes.dies[data.mes-1];
-    console.log(data.mes);
-    console.log(data.mes-1);
-    console.log(mes.dies[data.mes-1]);
-    console.log(mes.dies[0]);
 
     var dia=["Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte","Diumenge"];
     document.write('<div class="grid-container">');
